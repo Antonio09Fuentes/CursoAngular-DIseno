142b402ac2118492bcdd2c4f10d693ff67880bf2
= <101581240+Antonio09Fuentes@users.noreply.github.com>
2022-04-01 08:57:00 -0600

    Second commmit 4-1-22
    hmgmkgh,hh

---
+++ src/app/pages/nopagefound/nopagefound.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-nopagefound',
+  templateUrl: './nopagefound.component.html',
+  styleUrls: ['./nopagefound.component.css']
+})
+export class NopagefoundComponent  {
+
+  public  NowYear : number = new Date().getFullYear();
+}
